545dd0974d4db1a1e929bb88fdc22ef96dcdf20e
Jagadheesvaran T S <jagadheesvaran.t.s@gmail.com>
2019-01-13 22:53:46 -0700

    Added stats.c changes

--- course1/src/stats.c
+++ course1/src/stats.c
@@ -23,10 +23,12 @@
 
 #include <stdio.h>
 #include "stats.h"
+#include "platform.h"
 
 /* Size of the Data Set */
-#define SIZE (40)
+//#define SIZE (40)
 
+/*
 void main()
 {
  
@@ -35,34 +37,41 @@ void main()
                               200, 122, 150, 90,   92,  87, 177, 244,
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90}; 
-	printf("\nThe given array is\n");
-	print_array(test, SIZE);
+	#ifdef VERBOSE
+		PRINTF("\nThe given array is\n");
+		print_array(test, SIZE);
+	#endif
 	sort_array(test, SIZE);
-	printf("\nThe array after sorting in descending order is\n");
-	print_array(test, SIZE);
+	#ifdef VERBOSE
+		PRINTF("\nThe array after sorting in descending order is\n");
+		print_array(test, SIZE);
+	#endif
 	print_statistics(test, SIZE);
 
 }
+*/
 
 void print_statistics(unsigned char array[], unsigned int length)
 {
   	
-	printf("\n The Median is %u\n", find_median(array, length));
-	printf("\n The Mean is %u\n", find_mean(array, length));
-	printf("\n The Maximum is %u\n", find_maximum(array, length));
-	printf("\n The Minimum is %u\n", find_minimum(array, length));
+	PRINTF("\n The Median is %u\n", find_median(array, length));
+	PRINTF("\n The Mean is %u\n", find_mean(array, length);
+	PRINTF("\n The Maximum is %u\n", find_maximum(array, length));
+	PRINTF("\n The Minimum is %u\n", find_minimum(array, length));
 
 }
 
 void print_array(unsigned char array[], unsigned int length)
 {
 
-	unsigned int iterator;
-	for(iterator = 0; iterator < length; iterator++)
-	{
-		printf(" %u ", array[iterator]);
-	}
-	printf("\n");
+	#ifdef VERBOSE
+		unsigned int iterator;
+		for(iterator = 0; iterator < length; iterator++)
+		{
+			PRINTF(" %u ", array[iterator]);
+		}
+		PRINTF("\n");
+	#endif
 
 }
 
